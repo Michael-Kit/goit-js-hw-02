294a0763a06a8cfa10154c003848403613c3d254
Michael <captkit@ukr.net>
2025-05-17 23:03:59 +0300

    modefied js+task 1-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,26 @@
+// Функція checkForSpam(message) приймає рядок(параметр message), перевіряє його на вміст заборонених слів spam і sale, і 
+// повертає результат перевірки.Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+// Доповни код функції таким чином, що:
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її роботи.
+
+function checkForSpam(message) {
+  
+  let lowerCaseMessage = message.toLowerCase(); 
+  
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+}
 
 
-function getElementWidth(content, padding, border) {
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-  
-    const totalWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-    return totalWidth;
-  }
-  
-  console.log(getElementWidth("50px", "8px", "4px")); 
-  console.log(getElementWidth("60px", "12px", "8.5px")); 
-  console.log(getElementWidth("200px", "0px", "0px")); 
-  
+
+
